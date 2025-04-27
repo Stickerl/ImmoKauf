270ed58a0185416d5e7a23c053c6ef04486ebcf2
LESERSI <simoneleserer@gmail.com>
2025-04-27 22:28:46 +0200

    added new coloumns

--- Auswertung_Immobilien_Versuch2.sql
+++ Auswertung_Immobilien_Versuch2.sql
@@ -5,6 +5,8 @@ SELECT * FROM immobilienkauf.price_trend_per_city;
 
 ALTER TABLE immobilienkauf.copy_price_trend_per_city_w_calculations
 ADD COLUMN avg_percent_change DECIMAL(10, 8);
+ALTER TABLE immobilienkauf.copy_price_trend_per_city_w_calculations
+ADD COLUMN diff_from_avg DECIMAL(10, 8);
 
 SET SQL_SAFE_UPDATES = 0;
 
@@ -22,4 +24,9 @@ ON c.city_name = avg_data.city_name
 SET c.avg_percent_change = avg_data.avg_percent_change
 WHERE c.city_name IS NOT NULL; 
 
+UPDATE immobilienkauf.copy_price_trend_per_city_w_calculations
+SET diff_from_avg = price_change - avg_percent_change;
+
 SET SQL_SAFE_UPDATES = 1;
+
+
